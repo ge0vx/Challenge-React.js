0aa1f8ff3bc08902fe29ebbbafa7b9ccf8460fe9
ge0vx <geovapb@gmail.com>
2023-01-23 04:24:17 -0700

    adding routes definition, and Profile and Search pages

---
+++ web/utils/routes.ts
@@ -0,0 +1,37 @@
+import LogIn from "../pages/LogIn";
+import Profile from "../pages/Profile";
+import Search from "../pages/Search";
+
+import {FC} from "react";
+
+interface Route {
+    key: string,
+    title: string,
+    path: string,
+    enabled: boolean,
+    component: FC<{}>
+}
+
+export const routes: Array<Route> = [
+    {
+        key: 'login-route',
+        title: 'Log In',
+        path: '/',
+        enabled: true,
+        component: LogIn
+    },
+    {
+        key: 'profile-route',
+        title: 'Profile',
+        path: '/profile',
+        enabled: true,
+        component: Profile
+    },
+    {
+        key: 'search-route',
+        title: 'Search',
+        path: '/search',
+        enabled: true,
+        component: Search
+    }
+]
