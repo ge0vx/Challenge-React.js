42a2e12a110a46f1164c1abfc5bec8e2ab8d24e3
ge0vx <geovapb@gmail.com>
2023-01-23 02:42:06 -0700

    adding typescript, eslint, prettier and material ui icons

---
+++ web/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import LogIn from "./pages/LogIn";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LogIn />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
