155ce96ab8820a3655947327bfa1b994ff50d771
ge0vx <geovapb@gmail.com>
2023-01-24 01:57:47 -0700

    define action types

---
+++ web/store/types.ts
@@ -0,0 +1,9 @@
+const actionTypes = {
+  LOG_IN: "LOG_IN",
+  LOG_IN_SUCCESS: "LOG_IN_SUCCESS",
+  LOG_IN_FAILED: "LOG_IN_FAILED",
+  LOADING: "LOADING",
+  SEARCH_ITEMS: "REQUEST_ITEMS",
+};
+
+export default actionTypes;
